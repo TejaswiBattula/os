1c550ab04f57b0814f7025423f7ebc07387e35b0
TejaswiBattula <b.venkatteja@gmail.com>
2025-03-11 17:35:12 -0700

    PICs

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,6 +3,9 @@ use crate::println;
 use lazy_static::lazy_static;
 
 use crate::gdt;
+use pic8259::ChainedPics;
+use spin;
+
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -38,3 +41,12 @@ fn test_breakpoint_exception() {
     // invoke a breakpoint exception
     x86_64::instructions::interrupts::int3();
 }
+
+//--------------------------------
+// PICs
+//--------------------------------
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
