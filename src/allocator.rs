a9648259fcf21e148768b667dae3c2253979c589
TejaswiBattula <b.venkatteja@gmail.com>
2025-03-12 21:09:58 -0700

    linked list allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -2,9 +2,10 @@ use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 use linked_list_allocator::LockedHeap;
 use bump::BumpAllocator;
+use linked_list::LinkedListAllocator;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 extern crate alloc;
 
@@ -12,6 +13,7 @@ pub struct Dummy;
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 pub mod bump;
+pub mod linked_list;
 
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
